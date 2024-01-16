0e021c0b70038a9f7d537d2727bc741249680d45
wally <69739715+wallysaurus@users.noreply.github.com>
2024-01-16 12:51:20 -0600

    debug work

--- src/Main.kt
+++ src/Main.kt
@@ -4,15 +4,17 @@ object Main {
         val raw = expr.replace(Regex("/[()]/"), "")
         if (raw in listOf("TRUE", "FALSE")) return raw
 
-        var mutableExpr = expr
+        val mutableExpr = expr
             .replace("a", if (a) "TRUE" else "FALSE")
             .replace("b", if (b) "TRUE" else "FALSE")
-        val left = leftmostParenthesis(expr)
-        val right = correspondingRight(expr, left)
+        val left = leftmostParenthesis(mutableExpr)
+        val right = correspondingRight(mutableExpr, left)
+
         println("expr: $expr")
         println("mutableExpr: $mutableExpr")
+        println("leftIndex: $left, rightIndex: $right")
 
-        var section = mutableExpr.subSequence((left ?: -1)+1..<(right ?: expr.length)).toString()
+        val section = mutableExpr.subSequence((left ?: -1)+1..<(right ?: expr.length)-1).toString()
         return section
     }
 
@@ -22,7 +24,7 @@ object Main {
 
     private fun correspondingRight(expr: String, left: Int?) : Int? {
         if (left == null) return null
-        return (expr.length-1 downTo left).firstOrNull { expr[it] == ')' }
+        return (left ..< expr.length).firstOrNull { expr[it] == ')' }
     }
 
 }
