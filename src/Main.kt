00aa2320be7c896d2b42e70efbcfea67b45d9473
wallysaurus <wallyminehut@gmail.com>
2024-01-17 13:12:00 -0600

    brainrot

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,11 @@
 object Main {
 
-    fun case(a: Boolean, b: Boolean, expr: String) : String {
+    fun case(a: Boolean, b: Boolean, expr: String): String {
         val raw = expr.replace(Regex("/[()]/"), "")
         if (raw in listOf("TRUE", "FALSE")) return raw
 
-        val mutableExpr = expr
-            .replace("a", if (a) "TRUE" else "FALSE")
-            .replace("b", if (b) "TRUE" else "FALSE")
+        var mutableExpr = expr
+            .replace(" ", "")
         val left = leftmostParenthesis(mutableExpr)
         val right = correspondingRight(mutableExpr, left)
 
@@ -14,19 +13,31 @@ object Main {
         println("mutableExpr: $mutableExpr")
         println("leftIndex: $left, rightIndex: $right")
 
-        val section = mutableExpr.subSequence((left ?: -1)+1..<(right ?: expr.length)-1).toString()
-        return section
+        val section = mutableExpr.subSequence((left ?: -1) + 1..<(right ?: expr.length)).toString()
+        mutableExpr.replaceRange((left ?: -1), (right ?: expr.length), operate(section))
+        return mutableExpr
     }
 
-    private fun leftmostParenthesis(expr: String) : Int? {
-        return (expr.length-1 downTo 0).firstOrNull { expr[it] == '(' }
+    private fun leftmostParenthesis(expr: String): Int? {
+        return (expr.length - 1 downTo 0).firstOrNull { expr[it] == '(' }
     }
 
-    private fun correspondingRight(expr: String, left: Int?) : Int? {
+    private fun correspondingRight(expr: String, left: Int?): Int? {
         if (left == null) return null
-        return (left ..< expr.length).firstOrNull { expr[it] == ')' }
+        return (left..<expr.length).firstOrNull { expr[it] == ')' }
     }
 
+    // TRUE*FALSE
+    private fun operate(basicExpression: String, a: Boolean, b: Boolean): String {
+        with(basicExpression) {
+            return if (when {
+                this.contains("∧") -> { a && b }
+                this.contains("∨") -> { a || b }
+                this.contains("⊕") -> { a != b }
+                this.contains("=") ->
+            }) "T" else "F"
+        }
+    }
 }
 
 fun main() {
