d39f2a1526c7ac09cf4405d1650a4081eb3e1fd4
wally <69739715+WallyTube@users.noreply.github.com>
2024-01-17 23:19:15 -0600

    im craving waterburger rn tbh

--- src/Main.kt
+++ src/Main.kt
@@ -4,18 +4,20 @@ object Main {
         val raw = expr.replace(Regex("/[()]/"), "")
         if (raw in listOf("TRUE", "FALSE")) return raw
 
-        var mutableExpr = expr
-            .replace(" ", "")
-        val left = leftmostParenthesis(mutableExpr)
-        val right = correspondingRight(mutableExpr, left)
+        val left = leftmostParenthesis(expr)
+        val right = correspondingRight(expr, left)
 
-        println("expr: $expr")
-        println("mutableExpr: $mutableExpr")
-        println("leftIndex: $left, rightIndex: $right")
-
-        val section = mutableExpr.subSequence((left ?: -1) + 1..<(right ?: expr.length)).toString()
-        mutableExpr.replaceRange((left ?: -1), (right ?: expr.length), operate(section))
-        return mutableExpr
+        // grab the contents within the innermost parenthesis, excluding the parenthesis.
+        val section = expr.subSequence((left ?: -1) + 1..<(right ?: expr.length)).toString()
+        // use the operate() function to determine the result of the operation, and plug the value back into the equation.
+        try {
+            val result = expr.replaceRange((left ?: -1), (right ?: expr.length), operate(section, a, b))
+            return case(a, b, result)
+        } catch (error: Error) {
+            println(expr)
+        }
+        // recurse over the expression until it has been reduced to a singular value. (i.e. "T" or "(T)")
+        return case(a, b, "poo")
     }
 
     private fun leftmostParenthesis(expr: String): Int? {
@@ -27,14 +29,17 @@ object Main {
         return (left..<expr.length).firstOrNull { expr[it] == ')' }
     }
 
-    // TRUE*FALSE
+    // takes a pair of two values and an operator and returns the result of the operation.
+    // the two values are decided by order of operations in Main.case()
     private fun operate(basicExpression: String, a: Boolean, b: Boolean): String {
         with(basicExpression) {
             return if (when {
                 this.contains("∧") -> { a && b }
                 this.contains("∨") -> { a || b }
-                this.contains("⊕") -> { a != b }
-                this.contains("=") ->
+                this.contains("⊕") -> { a != b } // if a does not equal b, one value has to be true and one has to be false.
+                this.contains("=") -> { a == b }
+                this.contains("->") -> { !a || b }
+                else -> { false }
             }) "T" else "F"
         }
     }
